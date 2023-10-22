53dc2aba6d5dd047e201e85c843fa880d5339919
Ostap Ivakh <o.ivakh95@gmail.com>
2023-10-22 13:21:54 +0300

    Added views to planetarium app

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,151 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.db.models import F, Count
+from rest_framework import viewsets, mixins
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import GenericViewSet
+
+from planetarium.models import ShowTheme, PlanetariumDome, AstronomyShow, ShowSession, Reservation
+
+from planetarium.serializers import (
+    ShowThemeSerializer,
+    PlanetariumDomeSerializer,
+    AstronomyShowSerializer,
+    ShowSessionSerializer,
+    ShowSessionListSerializer,
+    AstronomyShowDetailSerializer,
+    ShowSessionDetailSerializer,
+    AstronomyShowListSerializer,
+    ReservationSerializer,
+    ReservationListSerializer,
+)
+
+
+class ShowThemeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class PlanetariumDomeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class AstronomyShowViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = AstronomyShow.objects.prefetch_related("show_themes")
+    serializer_class = AstronomyShowSerializer
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """Retrieve the movies with filters"""
+        title = self.request.query_params.get("title")
+        show_themes = self.request.query_params.get("show_themes")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if show_themes:
+            show_themes_ids = self._params_to_ints(show_themes)
+            queryset = queryset.filter(genres__id__in=show_themes_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+
+        return AstronomyShowSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = (
+        ShowSession.objects.all()
+        .select_related("astronomy_show", "planetarium_dome")
+        .annotate(
+            tickets_available=(
+                F("planetarium_dome__rows") * F("planetarium_dome__seats_in_row")
+                - Count("tickets")
+            )
+        )
+    )
+    serializer_class = ShowSessionSerializer
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_queryset(self):
+        date = self.request.query_params.get("date")
+        astronomy_show_id_str = self.request.query_params.get("astronomy_show")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(show_time__date=date)
+
+        if astronomy_show_id_str:
+            queryset = queryset.filter(astronomy_show_id=int(astronomy_show_id_str))
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowSessionListSerializer
+
+        if self.action == "retrieve":
+            return ShowSessionDetailSerializer
+
+        return ShowSessionSerializer
+
+
+class ReservationPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+
+
+class ReservationViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet,
+):
+    queryset = Reservation.objects.prefetch_related(
+        "tickets__show_session__astronomy_show", "tickets__show_session__planetarium_dome"
+    )
+    serializer_class = ReservationSerializer
+    pagination_class = ReservationPagination
+    # permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Reservation.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ReservationListSerializer
+
+        return ReservationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
